fbfd4eae59a56ec3de2280f999e24c28c6512acb
Chriszao <chris.f.assis18@gmail.com>
2021-09-24 21:53:45 -0300

    feat: Implemented findById and delete methods

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,17 +1,39 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [{
-  id: uuid(),
-  name: 'Chris',
-  email: 'chris.f.assis18@gmail.com',
-  phone: '123-456',
-  category_id: uuid(),
-}];
+let contacts = [
+  {
+    id: v4(),
+    name: 'Chris',
+    email: 'chris.f.assis18@gmail.com',
+    phone: '123-456',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Maria',
+    email: 'mariaEdu@gmail.com',
+    phone: '123-456',
+    category_id: v4(),
+  },
+];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      resolve(contacts = contacts.filter((contact) => contact.id !== id));
+      resolve();
     });
   }
 }
